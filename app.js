79bfc7f2f50f6c04e00e9e787c1f508c304a1fc0
cenk gürsoy <98879912+cnkgrsy@users.noreply.github.com>
2022-06-05 15:20:04 +0300

    data object added

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const fname = document.getElementById('fname')
-console.log(fname)
 
 const sname = document.getElementById('sname')
-console.log(sname)
 
 const eMail = document.getElementById('email')
-console.log(eMail)
-
 
 const BtnAdd = document.querySelector('.submit');
-console.log(BtnAdd);
-
 
 BtnAdd.addEventListener("click", AddNew);
 
     function AddNew() {
+        const clientinfo ={
+            fname :fname.value,
+            sname :sname.value,
+            eMail :eMail.value
+        }   
+        
      
-    console.log("add");
+    
         for(let i=0; i<3;i++){
             let newID = `div-created${i}`
             const newDiv = document.createElement('div');
@@ -34,13 +34,13 @@ BtnAdd.addEventListener("click", AddNew);
             newElement = document.getElementById(newID).appendChild(newH);
             
             if(i==0){
-                document.getElementById(newIDP).innerHTML = `Name ${fname.value}`;
+                document.getElementById(newIDP).innerHTML = `Name : ${clientinfo.fname}`;
                 console.log(fname)
             }else if(i==1){
-                document.getElementById(newIDP).innerHTML =  `Surname : ${sname.value}`;
+                document.getElementById(newIDP).innerHTML =  `Surname : ${clientinfo.sname}`;
 
             }else{
-                document.getElementById(newIDP).innerHTML =  `Email : ${eMail.value}`;;
+                document.getElementById(newIDP).innerHTML =  `Email : ${clientinfo.eMail}}`;;
 
             }
 
